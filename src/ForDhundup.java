d96c9e7607c58f0005ddf5fc8738668167fd52c0
Zixuan Zhang <39664230+HakkerBarry@users.noreply.github.com>
2020-04-11 00:31:21 -0600

    😃

--- src/ForDhundup.java
+++ src/ForDhundup.java
@@ -2,4 +2,8 @@ import java.util.ArrayList;
 
 public class ForDhundup {
 ArrayList<String> fakeShit = new ArrayList<>();
+public static void formLeo()
+{
+	String a = new String("hahahahahah");
+}
 }
