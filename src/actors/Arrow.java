89daa63e5ce2e3b92b54a9f91538987a7b4447e3
Zixuan Zhang <39664230+HakkerBarry@users.noreply.github.com>
2020-04-15 00:10:56 -0600

    😀 delete debug println

--- src/actors/Arrow.java
+++ src/actors/Arrow.java
@@ -34,7 +34,11 @@ public class Arrow extends Sprite{
 			}
 		}
 		//System.out.
+<<<<<<< Updated upstream
 		this.shiftPosition(new Point2D.Double(5,0));
+=======
+		this.shiftPosition(new Point2D.Double(15,0));
+>>>>>>> Stashed changes
 	}
 	
 	public void draw(Graphics g) {
