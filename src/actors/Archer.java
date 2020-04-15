7a9f861b9fd2fae064d33ad8c2d00b820a92b055
Zixuan Zhang <39664230+HakkerBarry@users.noreply.github.com>
2020-04-15 00:13:29 -0600

    Update Archer.java

--- src/actors/Archer.java
+++ src/actors/Archer.java
@@ -31,17 +31,11 @@ public class Archer extends Actor {
 		if(state == ATTACK)
 		{
 			if(coolDownCounter == this.coolDown) {//the final frame of attack anime
-<<<<<<< Updated upstream
-				System.out.println("a attack called");
-				this.setCurrentIgm(this.get(ATTACK, coolDown));
-				Arrow arrow = new Arrow(this.getPosition());
-				//add arrow to list in game
-				System.out.println(this.getPosition());
-=======
+
 				this.setCurrentIgm(this.get(ATTACK, coolDown));
 				Arrow arrow = new Arrow(new Point2D.Double(this.getPosition().getX(),this.getPosition().getY()));
 				//add arrow to list in game
->>>>>>> Stashed changes
+
 				Instance.getInstance().getGame().arrows.add(arrow);
 				this.resetCoolDown();
 				state = IDLE;
